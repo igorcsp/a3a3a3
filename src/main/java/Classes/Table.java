afc5ea48dfdf8872687275c6e5fcfdd26ed1098d
Igor Cordeiro de Souza Pereira - 823159742 <823159742@ulife.com.br>
2024-04-16 20:16:59 -0300

    kok

---
+++ src/main/java/Classes/Table.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author 823159742
+ */
+public class Table {
+    private String table;
+    private int columns;
+    private int rows;
+    
+    public Table(String table, int columns, int rows) {
+        this.table = table;
+        this.columns = columns;
+        this.rows = rows;
+    }
+    
+    
+}
